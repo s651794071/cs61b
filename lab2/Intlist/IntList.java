b00b6344ff645d937cb2ae02451589db49da57c2
Yang <49736123+s651794071@users.noreply.github.com>
2022-06-04 21:21:41 +0800

    fix bug since A might be null

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,8 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if (A == null)
+            return B;
         IntList T = A;
         while (T.rest != null){
             T = T.rest;
@@ -94,6 +96,8 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if (A == null)
+            return B;
         IntList res = new IntList(A.first, null);
         IntList tempA = A;
         IntList tempRes = res;
